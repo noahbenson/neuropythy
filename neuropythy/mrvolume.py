483ee213504f892464369df36d7ba5e28df7e4c9
Noah C. Benson <nben@nyu.edu>
2015-07-28 09:27:21 -0400

    Moved python files to a more sensible location in the repo. Removed inclusion of mrvolume, also added mrvolume.

---
+++ neuropythy/mrvolume.py
@@ -0,0 +1,10 @@
+####################################################################################################
+# mrvolume.py
+# Representations of MRI volumes
+# by Noah C. Benson
+
+import numpy as np
+import scipy as sp
+import nibabel.freesurfer.io as fsio
+import os
+
