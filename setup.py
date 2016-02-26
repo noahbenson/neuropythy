69e84540e34a03f97f055ece994f0b409a6bb6d5
Noah C. Benson <nben@nyu.edu>
2016-02-25 22:54:16 -0500

    Added package data.

---
+++ setup.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='neuropythy',
+    version='0.1.0-SNAPSHOT',
+    description='Toolbox for flexible cortical mesh analysis and registration',
+    keywords='neuroscience mesh cortex registration',
+    author='Noah C. Benson',
+    author_email='nben@nyu.edu',
+    url='https://github.com/noahbenson/neuropythy/',
+    license='GPLv3',
+    packages=['neuropythy'],
+    install_requires=[
+        'numpy>=1.2',
+        'scipy>=0.7',
+        'nibabel>=1.2',
+        'pysistence>=0.4'])
