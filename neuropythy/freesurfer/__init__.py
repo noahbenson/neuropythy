1e4458bfa0ef3131e0145a5662549bda4c86fd30
Noah C. Benson <nben@nyu.edu>
2016-02-27 21:04:25 -0500

    Modularized the freesurfer code

---
+++ neuropythy/freesurfer/__init__.py
@@ -0,0 +1,6 @@
+####################################################################################################
+# neuropythy/freesurfer/__init__.py
+# This file defines the FreeSurfer tools that are available as part of neuropythy.
+
+from .subject import (Subject, Hemisphere, cortex_to_ribbon_map, cortex_to_ribbon)
+
