fb2dee26dae49935fcfd4f3fe2dda314a91499d3
Noah C. Benson <nben@nyu.edu>
2016-03-18 16:57:53 -0400

    The registration namespace is getting closer: integration with Java now works to a limited extent, and the SchiraModel class seems functional.

--- neuropythy/registration.py
+++ neuropythy/registration.py
@@ -6,20 +6,27 @@
 import numpy as np
 import scipy as sp
 import os
+from numbers import Number
 from math import pi
 from cortex import CorticalMesh
+from pysistence import make_dict
 
 from py4j.java_gateway import (launch_gateway, JavaGateway, GatewayParameters)
 
 # Java start:
-__java_port = None
-__java = None
+_java_port = None
+_java = None
 def init_registration():
-    __java_port = launch_gateway(classpath=os.path.join(os.path.realpath(__file__), '..', 
-                                                        'lib', 'nben', 'target', 'nben-standalone.jar'),
-                                 javaopts=['-Xmx2g'],
-                                 die_on_exit=True)
-    __java = JavaGateway(gateway_parameters=GatewayParameters(port=__java_port))
+    global _java
+    if _java is not None:
+        return
+    _java_port = launch_gateway(
+        classpath=os.path.join(
+            os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
+            'lib', 'nben', 'target', 'nben-standalone.jar'),
+        javaopts=['-Xmx2g'],
+        die_on_exit=True)
+    _java = JavaGateway(gateway_parameters=GatewayParameters(port=_java_port))
 
 # These are dictionaries of all the details we have about each of the possible arguments to the
 # mesh_register's field argument:
@@ -59,6 +66,9 @@ def _parse_field_function_argument(argdat, args, faces, coords):
     return argdflt
 
 def _parse_field_argument(instruct):
+    global _java
+    if _java is None:
+        init_registration()
     if isinstance(instruct, basestring):
         insttype = instruct
         instargs = []
@@ -80,7 +90,7 @@ def _parse_field_argument(instruct):
             raise RuntimeError('Shape ' + shape_name + ' not supported for type ' + insttype)
         instdata = instdata[shape_name]
     # okay, we have a list of instructions... find the java method we are going to call...
-    java_method = getattr(__java.jvm.nben.mesh.registration.Fields, instdata[0])
+    java_method = getattr(_java.jvm.nben.mesh.registration.Fields, instdata[0])
     # and parse the arguments into a list...
     java_args = [_parse_field_function_argument(a, instargs, faces, coords) for a in instdata[1:]]
     # and call the function...
@@ -164,7 +174,8 @@ def mesh_register(mesh, field, max_steps=25000, max_step_size=0.1, max_pe_change
              ['anchor', 'Gaussian', [1, 10, 50], [[0.0, 0.0], [1.1, 1.1], [2.2, 2.2]]]],
             max_step_size=0.05,
             max_steps=10000)'''
-    if __java is None:
+    global _java
+    if _java is None:
         init_registration()
     # Sanity checking.
     # First, make sure that the arguments are all okay:
@@ -181,6 +192,84 @@ def mesh_register(mesh, field, max_steps=25000, max_step_size=0.1, max_pe_change
     coords = mesh.coordinates.tolist()
     potential = _parse_field_argument(field, faces, coords)
     # Okay, that's basically all we need to do the minimization...
-    minimizer = __java.jvm.nben.mesh.registration.Minimizer(potential, coords)
+    minimizer = _java.jvm.nben.mesh.registration.Minimizer(potential, coords)
     minimizer.step(max_pe_change, max_steps, max_step_size)
     return np.array(minimizer.getX())
+
+# How we construct a Schira Model:
+class SchiraModel:
+    '''
+    The SchiraModel class is a Python wrapper around the Java nben.neuroscience.SchiraModel class;
+    it handles conversion from visual field angle to cortical surface coordinates and vice versa for
+    the Banded Double-Sech model proposed in the following paper:
+    Schira MM, Tyler CW, Spehar B, Breakspear M (2010) Modeling Magnification and Anisotropy in the
+    Primate Foveal Confluence. PLOS Comput Biol 6(1):e1000651. doi:10.1371/journal.pcbi.1000651.
+    '''
+
+    # These are the accepted arguments to the model:
+    default_parameters = {
+        'A': 0.5,
+        'B': 135.0,
+        'lambda': 1.0,
+        'psi': 0.15,
+        'scale': [7.0, 8.0],
+        'shear': [[1.0, -0.2], [0.0, 1.0]],
+        'center': [-7.0, -2.0],
+        'v1size': 1.2,
+        'v2size': 0.6,
+        'v3size': 0.4,
+        'hv4size': 0.9,
+        'v3asize': 0.9}
+    # This function checks the given arguments to see if they are okay:
+    def __check_parameters(self, parameters):
+        # we don't care if there are extra parameters; we just make sure the given parameters make
+        # sense, then return the full set of parameters
+        opts = {
+            k: parameters[k] if k in parameters else v
+            for (k,v) in SchiraModel.default_parameters.iteritems()}
+        return opts
+
+    # This class is immutable: don't change the params to change the model; 
+    # don't change the java object!
+    def __setattr__(self, name, val):
+        raise ValueError('The SchiraModel class is immutable; its objects cannot be edited')
+
+    def __init__(self, **opts):
+        global _java
+        if _java is None:
+            init_registration()
+        # start by getting the proper parameters
+        params = self.__check_parameters(opts)
+        # Now, do the translations that we need...
+        if isinstance(params['scale'], Number):
+            params['scale'] = [params['scale'], params['scale']]
+        if isinstance(params['shear'], Number) and params['shear'] == 0:
+            params['shear'] = [[1, 0], [0, 1]]
+        elif params['shear'][0][0] != 1 or params['shear'][1][1] != 1:
+            raise RuntimeError('shear matrix [0,0] elements and [1,1] elements must be 1!')
+        if isinstance(params['center'], Number) and params['center'] == 0:
+            params['center'] = [0.0, 0.0]
+        self.__dict__['parameters'] = make_dict(params)
+        # Okay, let's construct the object...
+        self.__dict__['_java_object'] = _java.jvm.nben.neuroscience.SchiraModel(
+            params['A'],
+            params['B'],
+            params['lambda'],
+            params['psi'],
+            params['v1size'],
+            params['v2size'],
+            params['v3size'],
+            params['hv4size'],
+            params['v3asize'],
+            params['center'][0],
+            params['center'][1],
+            params['scale'][0],
+            params['scale'][1],
+            params['shear'][0][1],
+            params['shear'][1][0])
+    
+    def angle_to_cortex(self, theta, rho):
+        return self._java_object.angleToCortex(theta, rho)
+    def cortex_to_angle(self, x, y):
+        return self._java_object.cortexToAngle(x, y)
+
