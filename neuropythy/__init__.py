65fee6c513f5318c391450c60f9df0e19483eb66
Noah C. Benson <nben@nyu.edu>
2016-04-11 10:17:07 -0400

    Renamed neuropythy.subject to neuropythy.freesurfer_subject

--- neuropythy/__init__.py
+++ neuropythy/__init__.py
@@ -2,9 +2,9 @@
 
 '''Tools for analyzing and registering cortical meshes.'''
 
-from .cortex import (cortex_to_mrvolume, CorticalMesh)
+from cortex import (cortex_to_mrvolume, CorticalMesh)
 import freesurfer
-from .registration import mesh_register
+from registration import mesh_register
 
 # Version information...
 _version_major = 0
@@ -15,11 +15,11 @@ __version__ = "%s.%s.%s" % (_version_major, _version_minor, _version_micro)
 description = 'Integrate Python environment with FreeSurfer and perform mesh registration'
 
 # how to construct a freesurfer subject:
-def subject(name, subjects_dir=None):
+def freesurfer_subject(name, subjects_dir=None):
     '''
-    subject(name) yields a freesurfer Subject object for the subject with the given name. If the
-    given subject cannot be found, then a ValueError is raised. The option subjects_dir may be
-    given to specify the location of the subject directory.
+    freesurfer_subject(name) yields a freesurfer Subject object for the subject with the given name.
+    If the given subject cannot be found, then a ValueError is raised. The option subjects_dir may
+    be given to specify the location of the subject directory.
     '''
     if subjects_dir is None:
         return freesurfer.Subject(name)
