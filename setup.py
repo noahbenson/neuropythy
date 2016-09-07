c72064f73166cb09dee32b4a53371469d71e8c51
noahbenson <nben@nyu.edu>
2016-09-07 10:08:28 -0400

    fleshed out setup.py and updated version

--- setup.py
+++ setup.py
@@ -2,15 +2,45 @@
 
 from setuptools import setup
 
+# Deduce the version from the __init__.py file:
+version = None
+with open(os.path.join(os.path.dirname(__file__), 'neuropythy', '__init__.py'), 'r') as fid:
+    for line in (line.strip() for line in fid):
+        if line.startswith('__version__'):
+            version = line.split('=')[1].strip().strip('\'')
+            break
+if version is None: raise ValueError('No version found in neuropythy/__init__.py!')
+
 setup(
     name='neuropythy',
-    version='0.1.5',
+    version=version,
     description='Toolbox for flexible cortical mesh analysis and registration',
     keywords='neuroscience mesh cortex registration',
     author='Noah C. Benson',
     author_email='nben@nyu.edu',
-    url='https://github.com/noahbenson/neuropythy/',
+    maintainer_email='nben@nyu.edu',
+    long_description='''
+                     See the README.md file at the github repository for this package:
+                     https://github.com/noahbenson/neuropythy
+                     ''',
+    url='https://github.com/noahbenson/neuropythy',
+    download_url='https://github.com/noahbenson/neuropythy',
     license='GPLv3',
+    classifiers=['Development Status :: 2 - Pre-Alpha',
+                 'Intended Audience :: Science/Research',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+                 'Programming Language :: Python :: 2.7',
+                 'Topic :: Software Development',
+                 'Topic :: Software Development :: Libraries',
+                 'Topic :: Software Development :: Libraries :: Python Modules',
+                 'Topic :: Scientific/Engineering',
+                 'Topic :: Scientific/Engineering :: Information Analysis',
+                 'Topic :: Scientific/Engineering :: Medical Science Apps.',
+                 'Operating System :: Microsoft :: Windows',
+                 'Operating System :: POSIX',
+                 'Operating System :: Unix',
+                 'Operating System :: MacOS'],
     packages=['neuropythy',
               'neuropythy.util',
               'neuropythy.java',
@@ -22,10 +52,10 @@ setup(
               'neuropythy.registration',
               'neuropythy.vision',
               'neuropythy.commands'],
+    include_package_data=True,
     package_data={'': ['LICENSE.txt',
                        'neuropythy/lib/nben/target/nben-standalone.jar',
                        'neuropythy/lib/models/standard.fmm.gz']},
-    include_package_data=True,
     install_requires=['numpy>=1.2',
                       'scipy>=0.7',
                       'nibabel>=2.0',
