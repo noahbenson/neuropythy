fa4381159554525325dc608743f4a74e913f8108
noahbenson <nben@nyu.edu>
2017-04-04 17:57:17 -0400

    added isoangular_path function, which needs more testing

--- neuropythy/vision/__init__.py
+++ neuropythy/vision/__init__.py
@@ -9,5 +9,6 @@ from .retinotopy import (empirical_retinotopy_data, predicted_retinotopy_data, r
                          extract_retinotopy_argument,
                          register_retinotopy, retinotopy_anchors, retinotopy_model,
                          predict_retinotopy, register_retinotopy_initialize)
-from .cmag       import (neighborhood_cortical_magnification, path_cortical_magnification)
+from .cmag       import (neighborhood_cortical_magnification, path_cortical_magnification,
+                         isoangular_path)
 
