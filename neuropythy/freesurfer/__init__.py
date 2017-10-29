24b285e796b9b597a0b7c1ec0bbf1294e1fee9b0
noahbenson <nben@nyu.edu>
2017-10-29 11:49:15 -0400

    moved subject.py to core.py, rewrote freesurfer subject/cortex handling

--- neuropythy/freesurfer/__init__.py
+++ neuropythy/freesurfer/__init__.py
@@ -2,13 +2,9 @@
 # neuropythy/freesurfer/__init__.py
 # This file defines the FreeSurfer tools that are available as part of neuropythy.
 
-import os
-from .subject import (Subject, Hemisphere, 
-                      cortex_to_ribbon_map, cortex_to_ribbon, cortex_to_ribbon_map_lines,
-                      find_subject_path, subject_paths, add_subject_path)
+from .core import (FreeSurferSubject, 
+                   find_subject_path, subject_paths, add_subject_path, clear_subject_paths)
 
-# how to construct a freesurfer subject:
-__freesurfer_subjects = {}
 def freesurfer_subject(name):
     '''
     freesurfer_subject(name) yields a freesurfer Subject object for the subject with the given name.
@@ -17,12 +13,14 @@ def freesurfer_subject(name):
     means that you must create a transient version of the subject to modify it via the member
     function sub.transient().
     '''
+    import os
     subpath = find_subject_path(name)
     if subpath is None: return None
     fpath = '/' + os.path.relpath(subpath, '/')
-    if fpath in __freesurfer_subjects:
-        return __freesurfer_subjects[fpath]
+    if fpath in freesurfer_subject._cache:
+        return freesurfer_subject._cache[fpath]
     else:
-        sub = Subject(subpath).persist()
-        if isinstance(sub, Subject): __freesurfer_subjects[fpath] = sub
+        sub = FreeSurferSubject(subpath).persist()
+        if isinstance(sub, FreeSurferSubject): freesurfer_subject._cache[fpath] = sub
         return sub
+freesurfer_subject._cache = {}
