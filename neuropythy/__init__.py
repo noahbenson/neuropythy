3d034e35fca054213bdab007e458be1029370f0a
noahbenson <nben@nyu.edu>
2017-12-07 15:50:25 -0500

    fixed typo in __init__

--- neuropythy/__init__.py
+++ neuropythy/__init__.py
@@ -50,7 +50,7 @@ from io           import (load, save, to_nifti)
 from mri          import (Cortex, Subject)
 from vision       import (retinotopy_data, empirical_retinotopy_data, predicted_retinotopy_data,
                           register_retinotopy, retinotopy_anchors, retinotopy_model,
-                          neighborhood_cortical_magnification, as_retinotopy, mesh_retinotopy)
+                          neighborhood_cortical_magnification, as_retinotopy, retinotopy_data)
 from   freesurfer import (subject as freesurfer_subject, to_mgh)
 import freesurfer
 
