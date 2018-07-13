f0acef9bc3c90bc42c37ac59c0d8e5d96b4c7f9d
noahbenson <nben@nyu.edu>
2018-07-13 12:06:45 -0400

    to_mgh now auto-converts 64-bit integers into 32-bit integers since MGH files cannot store 64-bit

--- neuropythy/freesurfer/core.py
+++ neuropythy/freesurfer/core.py
@@ -461,6 +461,9 @@ def to_mgh(obj, like=None, header=None, affine=None, extra=Ellipsis):
     else:
         obj = np.asarray(obj)
     if len(obj.shape) < 3: obj = np.asarray([[obj]])
+    # make sure the dtype isn't a high-bit integer
+    if np.issubdtype(obj.dtype, np.integer) and obj.dtype.itemsize > 4:
+        obj = np.asarray(obj, dtype=np.int32)
     # Okay, make a new object now...
     obj = fsmgh.MGHImage(obj, affine, header=header, extra=extra)
     # Okay, that's it!
