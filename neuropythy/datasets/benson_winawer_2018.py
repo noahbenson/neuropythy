3caa477aefcda4cf8fc8d5b8436519ddbaa52a12
noahbenson <nben@nyu.edu>
2018-09-13 12:35:31 -0400

    added v123_table and some documentation

--- neuropythy/datasets/benson_winawer_2018.py
+++ neuropythy/datasets/benson_winawer_2018.py
@@ -3,14 +3,15 @@
 # The dataset from Benson and Winawer (2018); DOI: https://doi.org/10.1101/325597
 # by Noah C. Benson
 
-import os, six, shutil, urllib, tarfile, logging, pimms
+import os, six, shutil, urllib, tarfile, logging, warnings, pimms
 import numpy as np
 import pyrsistent as pyr
 
 if six.PY3: from functools import reduce
 
 from .core        import (Dataset, add_dataset)
-from ..util       import (config, curry)
+from ..util       import (config, curry, AutoDict)
+from ..vision     import as_retinotopy
 from ..           import io      as nyio
 from ..freesurfer import subject as freesurfer_subject
 
@@ -30,8 +31,69 @@ class BensonWinawer2018Dataset(Dataset):
     dset = neuropythy.data['benson_winawer_2018']
     sorted(dset.subjects.keys())
     #=> ['S1201', 'S1202', 'S1203', 'S1204', 'S1205', 'S1206', 'S1207', 'S1208']
-    sorted(dset.subjects['S1201'].lh.properties.keys())
 
+    dset.subjects['S1202']
+    #=> Subject(<S1202>, <'/Users/nben/.cache/benson_winawer_2018/freesurfer_subjects/S1202'>)
+
+    dset.subjects['S1202'].lh
+    #=> Cortex(<LH>, <301348 faces>, <150676 vertices>)
+
+    sorted(dset.subjects['S1201'].lh.properties.keys())
+    #=> ['convexity', 'curvature', 'index', 'inf-prf00_eccentricity', 'inf-prf00_polar_angle',
+    #=>  'inf-prf_radius', 'inf-prf_visual_area', 'label', 'midgray_surface_area',
+    #=>  'pial_surface_area', 'prf00_eccentricity', 'prf00_polar_angle', 'prf00_radius',
+    #=>  'prf00_variance_explained', 'prf01_eccentricity', 'prf01_polar_angle', 'prf01_radius',
+    #=>  'prf01_variance_explained', 'prf02_eccentricity', 'prf02_polar_angle', 'prf02_radius',
+    #=>  'prf02_variance_explained', 'prf03_eccentricity', 'prf03_polar_angle', 'prf03_radius',
+    #=>  'prf03_variance_explained', 'prf04_eccentricity', 'prf04_polar_angle', 'prf04_radius',
+    #=>  'prf04_variance_explained', 'prf05_eccentricity', 'prf05_polar_angle', 'prf05_radius',
+    #=>  'prf05_variance_explained', 'prf06_eccentricity', 'prf06_polar_angle', 'prf06_radius',
+    #=>  'prf06_variance_explained', 'prf07_eccentricity', 'prf07_polar_angle', 'prf07_radius',
+    #=>  'prf07_variance_explained', 'prf08_eccentricity', 'prf08_polar_angle', 'prf08_radius',
+    #=>  'prf08_variance_explained', 'prf09_eccentricity', 'prf09_polar_angle', 'prf09_radius',
+    #=>  'prf09_variance_explained', 'prf10_eccentricity', 'prf10_polar_angle', 'prf10_radius',
+    #=>  'prf10_variance_explained', 'prf11_eccentricity', 'prf11_polar_angle', 'prf11_radius',
+    #=>  'prf11_variance_explained', 'prf12_eccentricity', 'prf12_polar_angle', 'prf12_radius',
+    #=>  'prf12_variance_explained', 'prf13_eccentricity', 'prf13_polar_angle', 'prf13_radius',
+    #=>  'prf13_variance_explained', 'prf14_eccentricity', 'prf14_polar_angle', 'prf14_radius',
+    #=>  'prf14_variance_explained', 'prf15_eccentricity', 'prf15_polar_angle', 'prf15_radius',
+    #=>  'prf15_variance_explained', 'prf16_eccentricity', 'prf16_polar_angle', 'prf16_radius',
+    #=>  'prf16_variance_explained', 'prf17_eccentricity', 'prf17_polar_angle', 'prf17_radius',
+    #=>  'prf17_variance_explained', 'prf18_eccentricity', 'prf18_polar_angle', 'prf18_radius',
+    #=>  'prf18_variance_explained', 'prf19_eccentricity', 'prf19_polar_angle', 'prf19_radius',
+    #=>  'prf19_variance_explained', 'prf20_eccentricity', 'prf20_polar_angle', 'prf20_radius',
+    #=>  'prf20_variance_explained', 'prf21_eccentricity', 'prf21_polar_angle', 'prf21_radius',
+    #=>  'prf21_variance_explained', 'prf_eccentricity', 'prf_polar_angle', 'prf_radius',
+    #=>  'prf_variance_explained', 'prior-prf_eccentricity', 'prior-prf_polar_angle',
+    #=>  'prior-prf_radius', 'prior-prf_visual_area', 'thickness', 'volume', 'white_surface_area',
+    #=>  'wide-prf_eccentricity', 'wide-prf_polar_angle', 'wide-prf_radius',
+    #=>  'wide-prf_variance_explained']
+
+    dset.meta_data
+    #=> ['prf', 'prf00', 'prf01', 'prf02', 'prf03', 'prf04', 'prf05', 'prf06', 'prf07', 'prf08',
+    #=>  'prf09', 'prf10', 'prf11', 'prf12', 'prf13', 'prf14', 'prf15', 'prf16', 'prf17', 'prf18',
+    #=>  'prf19', 'prf20', 'prf21']
+
+    dset.meta_data['prf04']
+    #=> {'scan_seconds': 576, 'name': 'training14', 'id': 14, 'scans': 3}
+
+    # Note that the following lines will take awhile to calculate/load from cache due to the size
+    # of the data; additionally, the generated cache file is ~1GB.
+
+    dset.v123_table
+    #=> itable(('inf_x', 'label', 'radius', 'eccentricity', 'inf_radius', 'hemi', 'x', 'subject',
+    #=>         'y', 'inf_y', 'inf_eccentricity', 'midgray_surface_area', 'inf_polar_angle',
+    #=>         'polar_angle', 'inf_visual_area', 'pial_surface_area', 'dataset_id', 'dataset_name',
+    #=>         'variance_explained', 'white_surface_area'),
+    #=>        <50278184 rows>)
+
+    dset.v123_table[100]
+    #=> {'inf_x': 4.0969896, 'label': 100, 'radius': 2.096148, 'eccentricity': 3.3061967,
+    #=>  'inf_radius': 1.5482311, 'hemi': 'lh', 'x': 0.032615896, 'subject': 'S1208', 'y': -3.306036,
+    #=>  'inf_y': -2.0219889, 'inf_eccentricity': 4.5687814, 'midgray_surface_area': 1.072214,
+    #=>  'inf_polar_angle': 116.267746, 'polar_angle': 179.43477, 'inf_visual_area': 3,
+    #=>  'pial_surface_area': 1.3856983, 'dataset_id': 10, 'dataset_name': 'prf10',
+    #=>  'variance_explained': 0.054989286, 'white_surface_area': 0.75872976}
     '''
     dataset_urls = {'analyses':              'https://osf.io/cpfa8/download',
                     'retinotopy':            'https://osf.io/m4k8q/download',
@@ -202,6 +264,64 @@ class BensonWinawer2018Dataset(Dataset):
         # okay, next we want to setup the subjects
         return pimms.lazy_map({s:curry(BensonWinawer2018Dataset.load_subject, cache_directory, s)
                                for s in [('S12%02d' % s) for s in range(1,9)]})
+    @pimms.value
+    def v123_table(cache_directory, subjects):
+        '''
+        dataset.v123_table is a pimms ITable object for the BensonWinawer2018Dataset; the table
+        contains all relevant pRF data for all cortical surface vertices in the 8 subjects included
+        in the paper Benson and Winawer (2018).
+        '''
+        # First, see if there's a cache file
+        cachefl = os.path.join(cache_directory, 'v123_table.p')
+        if os.path.isfile(cachefl):
+            try: return pimms.load(cachefl)
+            except:
+                msg = 'neuropythy: Could not load existing v123_table cache file: %s' % cache_file
+                warnings.warn(msg)
+        # go through, building up arrays of arrays that we will concatenate at the end
+        data = AutoDict()
+        data.on_miss = lambda:[] # we want it to auto-produce lists...
+        # non-retinotopy props we want to add to the data...
+        props = ['midgray_surface_area', 'pial_surface_area', 'white_surface_area', 'label']
+        for (sid,sub) in six.iteritems(subjects):
+            for hname in ['lh','rh']:
+                hemi = sub.hemis[hname]
+                for (dskey,dsdata) in six.iteritems(BensonWinawer2018Dataset.prf_meta_data):
+                    dsid = 99 if dskey == 'prf' else int(dskey[3:])
+                    # okay, let's get the raw data we need to process...
+                    ang = hemi.prop(dskey + '_polar_angle')
+                    ecc = hemi.prop(dskey + '_eccentricity')
+                    # and the inferred data...
+                    iang = hemi.prop('inf-' + dskey + '_polar_angle')
+                    iecc = hemi.prop('inf-' + dskey + '_eccentricity')
+                    ilbl = hemi.prop('inf-' + dskey + '_visual_area')
+                    # process both of these (get x/y basically)
+                    (x, y)  = as_retinotopy({'polar_angle':ang,  'eccentricity':ecc},  'geographical')
+                    (ix,iy) = as_retinotopy({'polar_angle':iang, 'eccentricity':iecc}, 'geographical')
+                    # find the relevant vertices
+                    ii = np.where((iecc < 12) & np.sum([ilbl == k for k in (1,2,3)], axis=0))[0]
+                    # now add the relevant properties...
+                    for p in props: data[p].append(hemi.prop(p)[ii])
+                    for p0 in ['polar_angle', 'eccentricity', 'radius', 'variance_explained']:
+                        p = dskey + '_' + p0
+                        data[p0].append(hemi.prop(p)[ii])
+                    for (p,u) in zip(['x', 'y'], [x,y]):
+                        data[p].append(u)
+                    for p0 in ['_polar_angle', '_eccentricity', '_radius', '_visual_area']:
+                        p = 'inf-' + dskey + p0
+                        data['inf' + p0].append(hemi.prop(p)[ii])
+                    for (p0,u) in zip(['inf_x', 'inf_y'], [ix,iy]):
+                        data[p0].append(u)
+                    # we also want repeated properties for some things
+                    extras = {'subject':sid, 'hemi':hname, 'dataset_id':dsid, 'dataset_name':dskey}
+                    for (p,v) in six.iteritems(extras): data[p].append(np.full(len(ii), v))
+        # concatenate everything
+        data = pimms.itable({k:np.concatenate(v) for (k,v) in six.iteritems(data)})
+        if not os.path.isfile(cachefl):
+            # try to write out the cache file
+            try: pimms.save(cachefl, data)
+            except: pass
+        return data
     
 # we wrap this in a lambda so that it gets loaded when requested (in case the config changes between
 # when this gets run and when the dataset gets requested)
