0b7a546303e176f0dc41de39eeed87251e5d4ef4
noahbenson <nben@nyu.edu>
2018-10-08 17:47:43 -0400

    command-line is now in pimms

--- neuropythy/util/__init__.py
+++ neuropythy/util/__init__.py
@@ -2,7 +2,7 @@
 # neuropythy/util/__init__.py
 # This file defines the general tools that are available as part of neuropythy.
 
-from .core import (CommandLineParser, ObjectWithMetaData, to_affine, simplex_summation_matrix,
+from .core import (ObjectWithMetaData, to_affine, simplex_summation_matrix,
                    simplex_averaging_matrix, is_image, curry,
                    zinv, zdiv, times, divide, zdivide, plus, minus,
                    library_path, address_data, is_address, AutoDict,
